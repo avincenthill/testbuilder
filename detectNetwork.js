747d027a2e50270df2cfaab374c8db2b9b34efd5
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-06-20 18:38:47 -0700

    Add all work up to refactoring expect out of tests
    I've accidentally been working off a clone of the HR repo of
    testbuilder and not committing changes. This is where I've
    got to, which is through looping tests of Discover and
    Maestro networks.

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,18 +1,44 @@
-// Given a credit card number, this function should return a string with the 
-// name of a network, like 'MasterCard' or 'American Express'
-// Example: detectNetwork('343456789012345') should return 'American Express'
-
-// How can you tell one card network from another? Easy! 
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
-
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  const prefix = cardNumber[0] + cardNumber[1];
+  const threePrefix = prefix + cardNumber[2];
+  const fourPrefix = prefix + cardNumber[2] + cardNumber[3];
+  if (cardNumber.length === 14 && (prefix === "38" || prefix === "39")) {
+    return "Diner's Club";
+  } else if (cardNumber.length === 15 && (prefix === "34" || prefix === "37")) {
+    return "American Express";
+  } else if (
+    (cardNumber.length === 13 ||
+      cardNumber.length === 16 ||
+      cardNumber.length === 19) &&
+    cardNumber[0] === "4"
+  ) {
+    return "Visa";
+  } else if (
+    cardNumber.length === 16 &&
+    (prefix === "51" ||
+      prefix === "52" ||
+      prefix === "53" ||
+      prefix === "54" ||
+      prefix === "55")
+  ) {
+    return "MasterCard";
+  } else if (
+    (cardNumber.length === 16 || cardNumber.length === 19) &&
+    (prefix === "65" ||
+      (threePrefix >= "644" && threePrefix <= "649") ||
+      fourPrefix === "6011")
+  ) {
+    return "Discover";
+  } else if (
+    cardNumber.length > 11 &&
+    cardNumber.length < 20 &&
+    (fourPrefix === "5018" ||
+      fourPrefix === "5020" ||
+      fourPrefix === "5038" ||
+      fourPrefix === "6304")
+  ) {
+    return "Maestro";
+  } else {
+    return -1;
+  }
 };
-
-
