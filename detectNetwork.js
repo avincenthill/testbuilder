5127dd0e93078bbaa7c949f2fe55f00658ac1196
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-06-21 18:54:26 -0700

    Make China UnionPay tests pass

--- detectNetwork.js
+++ detectNetwork.js
@@ -2,6 +2,7 @@ var detectNetwork = function(cardNumber) {
   const prefix = cardNumber[0] + cardNumber[1];
   const threePrefix = prefix + cardNumber[2];
   const fourPrefix = prefix + cardNumber[2] + cardNumber[3];
+  const sixPrefix = fourPrefix + cardNumber[4] + cardNumber[5];
   if (cardNumber.length === 14 && (prefix === "38" || prefix === "39")) {
     return "Diner's Club";
   } else if (cardNumber.length === 15 && (prefix === "34" || prefix === "37")) {
@@ -38,7 +39,29 @@ var detectNetwork = function(cardNumber) {
       fourPrefix === "6304")
   ) {
     return "Maestro";
-  } else {
+  } else if (
+    //China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288
+    //and a length of 16-19.
+    cardNumber.length >= 16 &&
+    cardNumber.length <= 19 &&
+    ((parseInt(sixPrefix) >= 622126 && parseInt(sixPrefix) <= 622925) ||
+      (parseInt(threePrefix) >= 624 && parseInt(threePrefix) <= 626) ||
+      (parseInt(fourPrefix) >= 6282 && parseInt(fourPrefix) <= 6288))
+  ) {
+    return "China UnionPay";
+  }
+  // } else if (
+  //   // cardNumber.length > 11 &&
+  //   // cardNumber.length < 20 &&
+  //   // (fourPrefix === "5018" ||
+  //   //   fourPrefix === "5020" ||
+  //   //   fourPrefix === "5038" ||
+  //   //   fourPrefix === "6304")
+  //   true
+  // ) {
+  //   return "Switch";
+  // }
+  else {
     return -1;
   }
 };
